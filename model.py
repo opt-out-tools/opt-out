c65c11712653973d8b0e97df05d1fcb47f5f3825
malteserteresa <tess_the_titan@hotmail.com>
2019-03-25 13:27:35 +0100

    fixed failing tests and refactored escape_unicode and added to normalize function

--- model.py
+++ model.py
@@ -5,39 +5,39 @@ from clean import escape_unicode
 
 def normalize(comment):
     """
-       # lowercase, tokenize, remove punctuation
 
     """
-    tokenized_comment = list(map(lambda word: word.lower(), comment.split(' '))) # splits comment into words and makes each word lowercase
+    # splits comment into words and makes each word lowercase
+    tokenized_comment = list(map(lambda word: word.lower(), comment.split(" ")))
 
-    PUNCTUATION = r"""[!"#$%&()*+,-./:;<=>?@[]^_`{|}~']"""
+    cleaned_comment = escape_unicode(tokenized_comment)
 
-    return list(map(lambda word: word.translate(str.maketrans('', '', PUNCTUATION)), tokenized_comment)) # strips punctuation
-
-
-def test_prep_comment():
-    assert normalize("HELLO YOU! \\n") == ["hello", "you", "\\n"]
+    # strips punctuation & returns normalized comment
+    return list(map(lambda word: word.translate(str.maketrans("", "", r"""[!"#$%&()*+,-./:;<=>?@[]^_`{|}~']""")), cleaned_comment))
 
 
-def test_prep_comment2():
+def test_normalize_tokenize():
+    assert normalize("HELLO YOU") == ["hello", "you"]
+
+
+def test_normalize_unicode():
+    assert normalize("HELLO YOU! \\n") == ["hello", "you", "\n"]
+
+
+def test_normalize_punctuation():
     assert normalize("HELLO YOU! &") == ["hello", "you", ""]
-
-
-def test_prep_comment3():
-    assert normalize("\\xa0") == ["hello", "you", ""]
 
 
 if __name__ == '__main__':
     import os
 
     current_directory = os.getcwd()
-    test = pd.read_csv(current_directory + "/data/test_with_solutions.csv") # read in test data
     train = pd.read_csv(current_directory  + "/data/train.csv") # read in train data
+    test = pd.read_csv(current_directory + "/data/test_with_solutions.csv") # read in test data
 
-    train['Tokenized'] = train['Comment'].apply(escape_unicode)
-    train['Tokenized'] = train['Tokenized'].apply(normalize)
+    train['Tokenized'] = train['Comment'].apply(normalize)
 
     stopwords = np.loadtxt(current_directory + "/data/stopwords.txt", dtype=np.str) # read in stopwords
     train['Tokenized'] = train['Tokenized'].apply(lambda comment: [word for word in comment if word not in stopwords])
-    #
+
 
