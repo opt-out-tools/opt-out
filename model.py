2d3e1103ab6850f9524022a79b7e54ff5e6b60b6
malteserteresa <tess_the_titan@hotmail.com>
2019-03-25 14:56:06 +0100

    combined escaped unicode and normalize logic into one file and finished lookup dictionary

--- model.py
+++ model.py
@@ -1,43 +1,33 @@
 import pandas as pd
 import numpy as np
-from clean import escape_unicode
+# TODO needs improving
+from normalize import normalize
+from tensorflow import keras
 
 
-def normalize(comment):
+def enumerate_corpus(corpus):
+    """ Used to create a look up table for the vocabulary for later conversion back to the original sentence
     """
-
-    """
-    # splits comment into words and makes each word lowercase
-    tokenized_comment = list(map(lambda word: word.lower(), comment.split(" ")))
-
-    cleaned_comment = escape_unicode(tokenized_comment)
-
-    # strips punctuation & returns normalized comment
-    return list(map(lambda word: word.translate(str.maketrans("", "", r"""[!"#$%&()*+,-./:;<=>?@[]^_`{|}~']""")), cleaned_comment))
+    return {k: v for k, v in enumerate(corpus)}
 
 
-def test_normalize_tokenize():
-    assert normalize("HELLO YOU") == ["hello", "you"]
-
-
-def test_normalize_unicode():
-    assert normalize("HELLO YOU! \\n") == ["hello", "you", "\n"]
-
-
-def test_normalize_punctuation():
-    assert normalize("HELLO YOU! &") == ["hello", "you", ""]
+def test_enumerate_corpus():
+    assert enumerate_corpus(["figured", "apple"]) == {0: "figured", 1: "apple"}
 
 
 if __name__ == '__main__':
     import os
 
     current_directory = os.getcwd()
-    train = pd.read_csv(current_directory  + "/data/train.csv") # read in train data
-    test = pd.read_csv(current_directory + "/data/test_with_solutions.csv") # read in test data
+    train = pd.read_csv(current_directory + "/data/train.csv")
+    test = pd.read_csv(current_directory + "/data/test_with_solutions.csv")
 
-    train['Tokenized'] = train['Comment'].apply(normalize)
+    train["Tokenized"] = train["Comment"].apply(normalize)
 
-    stopwords = np.loadtxt(current_directory + "/data/stopwords.txt", dtype=np.str) # read in stopwords
-    train['Tokenized'] = train['Tokenized'].apply(lambda comment: [word for word in comment if word not in stopwords])
+    stopwords = np.loadtxt(current_directory + "/data/stopwords.txt", dtype=np.str)
+    train["Tokenized"] = train["Tokenized"].apply(lambda comment: [word for word in comment if word not in stopwords])
 
+    corpus = set([sentence for comment in train["Tokenized"].tolist() for sentence in comment])
+    dictionary = enumerate_corpus(corpus)
 
+    vocab_size = len(corpus)
--- normalize.py
+++ normalize.py
@@ -2,7 +2,7 @@ import re
 
 def escape_unicode(sentence):
     """ Breaks up a sentence and searches each word for unicode errors, converting them to the proper format
-    TODO \\xaO and \\n not being converted to should replace
+    TODO \\xaO and \\n not being converted to should replace - this still needs improving
     """
     return [word.encode('utf-8').decode('unicode_escape') if re.findall('\\\\\w+', word) != [] else word for word in sentence]
 
@@ -37,3 +37,28 @@ def test_escape_space():
 
 def test_escape_two_times():
     assert escape_unicode(['test\\n test\\n test']) == ['test\n test\n test']
+
+
+def normalize(comment):
+    """ Normailizes each comment, making the words lowercase, tokenizing them removing punctuation and escaping unicode characters
+
+    """
+    # splits comment into words and makes each word lowercase
+    tokenized_comment = list(map(lambda word: word.lower(), comment.split(" ")))
+
+    cleaned_comment = escape_unicode(tokenized_comment)
+
+    # strips punctuation & returns normalized comment
+    return list(map(lambda word: word.translate(str.maketrans("", "", r"""[!"#$%&()*+,-./:;<=>?@[]^_`{|}~']""")), cleaned_comment))
+
+
+def test_normalize_tokenize():
+    assert normalize("HELLO YOU") == ["hello", "you"]
+
+
+def test_normalize_unicode():
+    assert normalize("HELLO YOU! \\n") == ["hello", "you", "\n"]
+
+
+def test_normalize_punctuation():
+    assert normalize("HELLO YOU! &") == ["hello", "you", ""]
