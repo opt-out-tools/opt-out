adf9b519e4aa55a2415a0834cb281c2799cd1d4c
malteserteresa <tess_the_titan@hotmail.com>
2019-04-02 12:42:58 +0200

    Completed the niave implementation of NN text classification model and improved the normalization step greatly

--- model.py
+++ model.py
@@ -20,13 +20,14 @@ if __name__ == '__main__':
 
     train["Tokenized"] = train["Comment"].apply(normalize)
 
+
     stopwords = np.loadtxt(current_directory + "/data/stopwords.txt", dtype=np.str)
     train["Tokenized"] = train["Tokenized"].apply(lambda comment: [word for word in comment if word not in stopwords])
 
     corpus = set([sentence for comment in train["Tokenized"].tolist() for sentence in comment])
     dictionary = enumerate_corpus(corpus)
     train["Enumerated"] = train["Tokenized"].apply((lambda comment: enumerate_comment(comment,dictionary)))
-
+    train.to_csv(current_directory + "cleaned.csv")
     vocab_size = len(corpus)
 
     train_data = keras.preprocessing.sequence.pad_sequences(train["Enumerated"].values, padding='post')
@@ -41,4 +42,48 @@ if __name__ == '__main__':
 
     model.compile(optimizer='adam', loss='binary_crossentropy',  metrics=['acc'])
 
-   
+    mid = int(3947/2)
+    x_val = train_data[:mid]
+    partial_x_train = train_data[mid:]
+
+
+    y_val = train['Insult'][:mid]
+    partial_y_train = train['Insult'][mid:]
+
+    history = model.fit(partial_x_train, partial_y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+   # Evaluate
+
+    history_dict = history.history
+    history_dict.keys()
+
+    import matplotlib.pyplot as plt
+
+    acc = history_dict['acc']
+    val_acc = history_dict['val_acc']
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    # "bo" is for "blue dot"
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    # b is for "solid blue line"
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
+
+    plt.clf()  # clear figure
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
--- normalize.py
+++ normalize.py
@@ -1,55 +1,77 @@
 import re
 
+# TODO refactor to do and more tests with different unicode characters and when characters are near numbers
+
 def escape_unicode(sentence):
-    """ Breaks up a sentence and searches each word for unicode errors, converting them to the proper format
-    TODO \\xaO and \\n not being converted to should replace - this still needs improving
+    """ Breaks up a sentence and searches each word for unicode characters, converting them to their encoded form
     """
-    return [word.encode('utf-8').decode('unicode_escape') if re.findall('\\\\\w+', word) != [] else word for word in sentence]
+    return [word.encode('utf-8').decode('unicode_escape') if re.findall('\\\\\w+', word) != [] else word for word in
+            sentence]
 
 
-def test_escape_whitespace():
-    assert escape_unicode(['\\ntest']) == ['\ntest']
+def test_escape_a_acute():
+    assert escape_unicode(['\\xe1']) == ['á']
 
 
-def test_escape_hex():
-    assert escape_unicode(['\\xa0']) == ['\xa0']
+def test_escape_a_umlaut():
+    assert escape_unicode(['\\xe4']) == ['ä']
 
 
-def test_escape_beginning():
-    assert escape_unicode(['\\xa0test test']) == ['\xa0test test']
+def test_escape_inverted_question_mark():
+    assert escape_unicode(['\\xbf']) == ['¿']
+
+def replace_spaces(sentence):
+    """ Replaces the unicode chcarcters for whitespaces ad new lines with spaces
+    """
+    pattern = '(\\\\xa0)|(\\\\n)|(\\\\xc2)'
+    return [re.sub(pattern, " ", word) if re.findall(pattern, word) != [] else word for word in sentence]
 
 
-def test_escape_middle():
-    assert escape_unicode(['test\\xa0test']) == ['test\xa0test']
+def test_replace_whitespace():
+    assert replace_spaces(['\\ntest']) == [' test']
 
 
-def test_escape_middle_twice():
-    assert escape_unicode(['test\\xa0\\xa0test']) == ['test\xa0\xa0test']
+def test_replace_beginning():
+    assert replace_spaces(['\\xa0test test']) == [' test test']
 
 
-def test_escape_end():
-    assert escape_unicode(['test\\xa0']) == ['test\xa0']
+def test_replace_middle():
+    assert replace_spaces(['test\\xa0test']) == ['test test']
 
 
-def test_escape_space():
-    assert escape_unicode(['test\\n test']) == ['test\n test']
+def test_replace_middle_twice():
+    assert replace_spaces(['test\\xa0\\xa0test']) == ['test  test']
 
 
-def test_escape_two_times():
-    assert escape_unicode(['test\\n test\\n test']) == ['test\n test\n test']
+def test_replace_end():
+    assert replace_spaces(['test\\xa0']) == ['test ']
+
+
+def test_replace_space():
+    assert replace_spaces(['test\\n test']) == ['test  test']
+
+
+def test_replace_two_times():
+    assert replace_spaces(['test\\n test\\n test']) == ['test  test  test']
+
+
+def test_replace_different_unicode():
+    assert replace_spaces(['test\\xc2\\xa0test\\n test']) == ['test  test  test']
 
 
 def normalize(comment):
     """ Normailizes each comment, making the words lowercase, tokenizing them removing punctuation and escaping unicode characters
-
     """
     # splits comment into words and makes each word lowercase
     tokenized_comment = list(map(lambda word: word.lower(), comment.split(" ")))
 
-    cleaned_comment = escape_unicode(tokenized_comment)
+    cleaned_comment = replace_spaces(tokenized_comment)
+    cleaned_comment = escape_unicode(cleaned_comment)
+
 
     # strips punctuation & returns normalized comment
-    return list(map(lambda word: word.translate(str.maketrans("", "", r"""[!"#$%&()*+,-./:;<=>?@[]^_`{|}~']""")), cleaned_comment))
+    return list(map(lambda word: word.translate(str.maketrans("", "", r"""[!"#$%&()*+,-./:;<=>?@[]^_`{|}~'\(\\\\\)¿]""")),
+                    cleaned_comment))
 
 
 def test_normalize_tokenize():
@@ -57,8 +79,12 @@ def test_normalize_tokenize():
 
 
 def test_normalize_unicode():
-    assert normalize("HELLO YOU! \\n") == ["hello", "you", "\n"]
+    assert normalize("HELLO YOU! \\n") == ["hello", "you", " "]
 
 
 def test_normalize_punctuation():
     assert normalize("HELLO YOU! &") == ["hello", "you", ""]
+
+
+def test_normalize_double_backslash():
+    assert normalize("HELLO YOU\\") == ["hello", "you"]
