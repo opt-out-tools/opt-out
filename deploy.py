6711b8fe42515046c6de39f6f90eef2cc75d74ff
malteserteresa <tess_the_titan@hotmail.com>
2019-04-14 15:20:22 +0200

    Added models directory

--- deploy.py
+++ deploy.py
@@ -14,14 +14,14 @@ app = Flask(__name__)
 @app.route("/test")
 def generate_sentiment_score():
     """Returns the sentiment score of the parsed sentence.
-    Args:
+    
     Return:
         score (int) : The sentiment score of the sentence. 1 - abusive, 0 - not abusive.
     """
     train_data = pd.read_csv(os.getcwd() + "/data/DataTurks/dump.csv")
     corpus_vocabulary = create_dictionary(train_data['content'])
 
-    model = load_model("model_120.h5")
+    model = load_model("stored_models/model_120.h5")
     model._make_predict_function()
 
     sentence = request.args.get("sentence")
