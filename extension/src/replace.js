77ea9e6d0d01b619feac1314054cec66faf19058
malteserteresa <tess_the_titan@hotmail.com>
2019-04-29 15:31:23 +0200

    Browswer extension set up for OSD workshop

---
+++ extension/src/replace.js
@@ -0,0 +1,25 @@
+
+// function getElement(document) {
+
+// }
+
+// var getTweets = function() {
+
+// }
+
+// var getSentimentScore = function() {
+//   return 0.5;
+// }
+
+// var modifyHatefulTweet = function() {
+
+// }
+function add (a, b) {
+  // return the sum of the arguments
+  return a + b;
+
+}
+
+  module.exports = {
+    add
+ }
---
+++ extension/test/replace.test.js
@@ -0,0 +1,11 @@
+const {expect} = require('chai');
+const {
+    add 
+} = require('../src/replace');
+
+describe('replace', () => {
+    describe('Find elements containing tweets', () => {
+      it('returns elements', () => {
+        expect(add(2,3)).to.equal(5);
+        expect(add(-5, 6)).to.equal(1);
+      });} ) } )
