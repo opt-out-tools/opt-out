b746ae9bd692a4d43e198865db1f4d4b38b4a22c
malteserteresa <tess_the_titan@hotmail.com>
2019-10-19 13:38:48 +0200

    added linted files and package.json

--- extension/content/opt-out-ext.js
+++ extension/content/opt-out-ext.js
@@ -2,103 +2,89 @@ let selector;
 const root = document.getElementById('doc') || document.getElementById('react-root');
 
 if (document.querySelector('body').classList.contains('logged-out')) {
-    console.log('offline');
-    selector = '.tweet';
+  console.log('offline');
+  selector = '.tweet';
 } else {
-    console.log('online');
-    selector = '[data-testid="tweet"]';
+  console.log('online');
+  selector = '[data-testid="tweet"]';
 }
 /*
 function which calls server for given node, and depending on the response, applies pre-defined action
  */
-let checkText = function(node) {
-    console.log('Sending Request');
-    let link = "https://api.optoutools.com/predict";
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', link, true);
-    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-    xhr.withCredentials = true;
-    xhr.onreadystatechange = function (e) {
-        if (xhr.readyState !== 4) {
-            return;
-        }
-        if (xhr.status === 200) {
-            console.log("Response received as ", JSON.parse(xhr.response).predictions[0]);
-            if (JSON.parse(xhr.response).predictions[0]){
-                node.classList.add("processed-true");
-                let tweetText = node.querySelector(selector + ' > div ~ div > div ~ div');
-                styleTweet(tweetText, option);
-            }
-            else {
-                node.classList.add("processed-false");
-            }
-        } else {
-            console.log("Failed response", xhr);
-        }
-    };
-    xhr.send(
-        JSON.stringify({
-            texts: [node.innerText]
-        })
-    );
+const checkText = function (node) {
+  console.log('Sending Request');
+  const link = 'https://api.optoutools.com/predict';
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', link, true);
+  xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+  xhr.withCredentials = true;
+  xhr.onreadystatechange = function (e) {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+    if (xhr.status === 200) {
+      console.log('Response received as ', JSON.parse(xhr.response).predictions[0]);
+      if (JSON.parse(xhr.response).predictions[0]) {
+        node.classList.add('processed-true');
+        const tweetText = node.querySelector(`${selector} > div ~ div > div ~ div`);
+        styleTweet(tweetText, option);
+      } else {
+        node.classList.add('processed-false');
+      }
+    } else {
+      console.log('Failed response', xhr);
+    }
+  };
+  xhr.send(
+    JSON.stringify({
+      texts: [node.innerText],
+    }),
+  );
 };
 
 /*
 Depending on `option` sets classes to tweet nodes
  */
-let option = "tw";
-styleTweet = function(element, option) {
-    if (option.includes('tw'))
-        element.classList.add("opt-out-tw");
-    else
-        element.classList.remove("opt-out-tw");
-    if (option.includes('tc'))
-        element.classList.add("opt-out-tc");
-    else
-        element.classList.remove("opt-out-tc");
-    if (option.includes('tr'))
-        element.classList.add("opt-out-trem");
-    else
-        element.classList.remove("opt-out-trem");
+let option = 'tw';
+styleTweet = function (element, option) {
+  if (option.includes('tw')) element.classList.add('opt-out-tw');
+  else element.classList.remove('opt-out-tw');
+  if (option.includes('tc')) element.classList.add('opt-out-tc');
+  else element.classList.remove('opt-out-tc');
+  if (option.includes('tr')) element.classList.add('opt-out-trem');
+  else element.classList.remove('opt-out-trem');
 };
 
 /*
 * Predefines action and changes it depending on user action
  */
 browser.runtime.onMessage.addListener((message) => {
-    if (option !== message.command){
-        option = message.command;
-        let posts = document.getElementsByClassName("processed-true");
-        for (let i = 0; i <  posts.length || 0 ; i++ | 0) {
-            let tweetText = posts[i].querySelector(selector + ' > div ~ div > div ~ div');//selecting text inside tweet
-            styleTweet(tweetText, option);
-        }
+  if (option !== message.command) {
+    option = message.command;
+    const posts = document.getElementsByClassName('processed-true');
+    for (let i = 0; i < posts.length || 0; i++ | 0) {
+      const tweetText = posts[i].querySelector(`${selector} > div ~ div > div ~ div`);// selecting text inside tweet
+      styleTweet(tweetText, option);
     }
+  }
 });
 
 
-let processTweets = function () {
-    let posts = document.querySelectorAll(selector);//selecting tweet object
-    for (let i = 0; i < posts.length || 0; i++ | 0) {
-        if (posts[i].classList.contains('processed-true'))
-            continue;
-        if (posts[i].classList.contains('processed-false'))
-            continue;
-        checkText(posts[i]);
-    }
+const processTweets = function () {
+  const posts = document.querySelectorAll(selector);// selecting tweet object
+  for (let i = 0; i < posts.length || 0; i++ | 0) {
+    if (posts[i].classList.contains('processed-true')) continue;
+    if (posts[i].classList.contains('processed-false')) continue;
+    checkText(posts[i]);
+  }
 };
 
 const checkTweetList = function (mutationsList) {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            processTweets();
-        }
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      processTweets();
     }
+  }
 };
 const checkTweetListObserver = new MutationObserver(checkTweetList);
-checkTweetListObserver.observe(root, {childList: true, subtree: true});
-
-
-
-
-
+checkTweetListObserver.observe(root, { childList: true, subtree: true });
--- extension/popup/set_view_mod.js
+++ extension/popup/set_view_mod.js
@@ -1,34 +1,34 @@
 document.console.log('ss');
 function listenForClicks() {
-    function setChecks(tabs) {
-        let msg =[];
-        if (document.getElementById('text_white').checked){
-            msg.push("tw");
-        }
-        if (document.getElementById('text_crossed').checked){
-            msg.push("tc");
-        }
-        if (document.getElementById('text_removed').checked){
-            msg.push("tr");
-        }
-        msg = msg.join();
-        function onError(error) {
-            alert(`Error: ${error}`);
-        }
-        browser.storage.sync.set({style: msg});
-        browser.tabs.sendMessage(tabs[0].id, {
-            command: msg,
-        });
+  function setChecks(tabs) {
+    let msg = [];
+    if (document.getElementById('text_white').checked) {
+      msg.push('tw');
     }
-
-    browser.tabs.query({active: true, currentWindow: true})
-        .then(setChecks)
-        .catch(reportError);
-
-    function reportError(error) {
-        console.error(`Could not opt-out: ${error}`);
-        // reportExecuteScriptError(error)
+    if (document.getElementById('text_crossed').checked) {
+      msg.push('tc');
     }
+    if (document.getElementById('text_removed').checked) {
+      msg.push('tr');
+    }
+    msg = msg.join();
+    function onError(error) {
+      alert(`Error: ${error}`);
+    }
+    browser.storage.sync.set({ style: msg });
+    browser.tabs.sendMessage(tabs[0].id, {
+      command: msg,
+    });
+  }
+
+  browser.tabs.query({ active: true, currentWindow: true })
+    .then(setChecks)
+    .catch(reportError);
+
+  function reportError(error) {
+    console.error(`Could not opt-out: ${error}`);
+    // reportExecuteScriptError(error)
+  }
 }
 
 /**
@@ -36,25 +36,24 @@ function listenForClicks() {
  * Display the popup's error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
-    document.querySelector("#popup-content").classList.add("hidden");
-    document.querySelector("#error-content").classList.remove("hidden");
-    console.error(`Failed to execute opt-out content script: ${error.message}`);
+  document.querySelector('#popup-content').classList.add('hidden');
+  document.querySelector('#error-content').classList.remove('hidden');
+  console.error(`Failed to execute opt-out content script: ${error.message}`);
 }
 
 function restoreOptions() {
+  function setCurrentChoice(result) {
+    document.querySelector('#text_white').checked = result.split(',').includes('tw');
+    document.querySelector('#text_crossed').checked = result.split(',').includes('tc');
+    document.querySelector('#text_removed').checked = result.split(',').includes('tr');
+  }
 
-    function setCurrentChoice(result) {
-        document.querySelector("#text_white").checked = result.split(",").includes('tw');
-        document.querySelector("#text_crossed").checked = result.split(",").includes('tc');
-        document.querySelector("#text_removed").checked = result.split(",").includes('tr');
-    }
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
 
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
-
-    let getting = browser.storage.sync.get("style");
-    getting.then(setCurrentChoice, onError);
+  const getting = browser.storage.sync.get('style');
+  getting.then(setCurrentChoice, onError);
 }
 
 /**
@@ -62,5 +61,5 @@ function restoreOptions() {
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.addEventListener("click", listenForClicks);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener('click', listenForClicks);
