1051a2f7d7eb56c98d94bf2b256e81e55ac28519
malteserteresa <tea.in.grams@gmail.com>
2019-10-19 16:39:27 +0200

    Revert "Extend Circle CI config"

--- extension/popup/set_view_mod.js
+++ extension/popup/set_view_mod.js
@@ -1,69 +1,60 @@
 document.console.log('ss');
+function listenForClicks() {
+    function setChecks(tabs) {
+        let msg =[];
+        if (document.getElementById('text_white').checked){
+            msg.push("tw");
+        }
+        if (document.getElementById('text_crossed').checked){
+            msg.push("tc");
+        }
+        if (document.getElementById('text_removed').checked){
+            msg.push("tr");
+        }
+        msg = msg.join();
+        function onError(error) {
+            alert(`Error: ${error}`);
+        }
+        browser.storage.sync.set({style: msg});
+        browser.tabs.sendMessage(tabs[0].id, {
+            command: msg,
+        });
+    }
+
+    browser.tabs.query({active: true, currentWindow: true})
+        .then(setChecks)
+        .catch(reportError);
+
+    function reportError(error) {
+        console.error(`Could not opt-out: ${error}`);
+        // reportExecuteScriptError(error)
+    }
+}
+
 /**
  * There was an error executing the script.
  * Display the popup's error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
-  document.querySelector('#popup-content').classList.add('hidden');
-  document.querySelector('#error-content').classList.remove('hidden');
-  console.error(`Failed to execute opt-out content script: ${error.message}`);
-}
-
-function listenForClicks() {
-  function setChecks(tabs) {
-    let msg = [];
-    if (document.getElementById('text_white').checked) {
-      msg.push('tw');
-    }
-    if (document.getElementById('text_crossed').checked) {
-      msg.push('tc');
-    }
-    if (document.getElementById('text_removed').checked) {
-      msg.push('tr');
-    }
-    msg = msg.join();
-    // disable eslint error for browser
-    // eslint-disable-next-line no-undef
-    browser.storage.sync.set({ style: msg });
-    // eslint-disable-next-line no-undef
-    browser.tabs.sendMessage(tabs[0].id, {
-      command: msg,
-    });
-  }
-
-  function reportError(error) {
-    console.error(`Could not opt-out: ${error}`);
-    reportExecuteScriptError(error);
-  }
-
-  // eslint-disable-next-line no-undef
-  browser.tabs
-    .query({ active: true, currentWindow: true })
-    .then(setChecks)
-    .catch(reportError);
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute opt-out content script: ${error.message}`);
 }
 
 function restoreOptions() {
-  function setCurrentChoice(result) {
-    document.querySelector('#text_white').checked = result
-      .split(',')
-      .includes('tw');
-    document.querySelector('#text_crossed').checked = result
-      .split(',')
-      .includes('tc');
-    document.querySelector('#text_removed').checked = result
-      .split(',')
-      .includes('tr');
-  }
 
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
+    function setCurrentChoice(result) {
+        document.querySelector("#text_white").checked = result.split(",").includes('tw');
+        document.querySelector("#text_crossed").checked = result.split(",").includes('tc');
+        document.querySelector("#text_removed").checked = result.split(",").includes('tr');
+    }
 
-  // disable eslint error for browser
-  // eslint-disable-next-line no-undef
-  const getting = browser.storage.sync.get('style');
-  getting.then(setCurrentChoice, onError);
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    let getting = browser.storage.sync.get("style");
+    getting.then(setCurrentChoice, onError);
 }
 
 /**
@@ -71,5 +62,5 @@ function restoreOptions() {
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.addEventListener('click', listenForClicks);
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.addEventListener("click", listenForClicks);
