bc0b0f1a6ee3373783aeec587ebb4239139a3947
Sarah Köhler <sarah.c.koehler@gmail.com>
2019-10-28 22:33:24 +0100

    clean up test for reset and add test for input

--- extension/popup/set_view_mod.test.js
+++ extension/popup/set_view_mod.test.js
@@ -1,28 +1,27 @@
 const restoreOptions = require('./reset');
 
-describe('test popup code', () => {
-  let fakeGet;
-  let fakeBrowser;
+describe('test reset options function of popup code', () => {
+  const defaultId = 'text_crossed';
+  const selector = `#${defaultId}`;
+
   beforeEach(async () => {
-    // fake the browser
-    fakeGet = jest.fn((text) => text);
-    fakeBrowser = { storage: { sync: { get: fakeGet } } };
-    global.browser = fakeBrowser;
-
-    // add elements to select
-    document.body.innerHTML = '<div id="text_crossed"></div>';
-
-    // wait a tick to give the production code the chance to execute
-    return new Promise((resolve) => process.nextTick(resolve));
+    // add input to select
+    document.body.innerHTML = `<input type="radio" id="${defaultId}" >`;
   });
 
-  it(' restoreOption should call browser.storage.sync.get once', () => {
-    restoreOptions();
-    expect(fakeGet.mock.calls.length).toBe(1);
-  });
-
-  it('should call browser API', () => {
+  // test using WebExtension mock
+  it('reset should call browser storage API', () => {
     restoreOptions();
     expect(browser.storage.sync.get).toHaveBeenCalled();
   });
+
+  it('reset should select the default option', async () => {
+    const input = document.querySelector(selector);
+    // mock storage should be empty, so reset should select the default option
+    expect(input.checked).toBe(false);
+    await restoreOptions();
+    expect(input.checked).toBe(true);
+  });
+
+  // TODO: Add test for selecting a different case than the default
 });
