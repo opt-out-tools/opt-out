ef6d0461b3a27a85b3184e2dd63fe6089a56c630
Nikola Ribic <nikola.ribic@softerrific.com>
2020-01-10 15:38:17 +0100

    - removed decimals and added knob images, updated CSS

--- extension/popup/functions/updateSliderKnob.js
+++ extension/popup/functions/updateSliderKnob.js
@@ -4,5 +4,26 @@
  * @param slider
  */
 export default function (slider) {
-  (parseFloat(slider.value) >= 0.5) ? slider.classList.remove('slider-angry') : slider.classList.add('slider-angry');
+  // (parseFloat(slider.value) >= 0.5) ? slider.classList.remove('slider-angry') : slider.classList.add('slider-angry');
+  slider.classList.remove(...slider.classList);
+  switch (true) {
+    case (parseFloat(slider.value) >= 1) :
+      slider.classList.add('slider-happy');
+      break;
+    case (parseFloat(slider.value) >= 0.9) :
+      slider.classList.add('slider-09');
+      break;
+    case (parseFloat(slider.value) >= 0.8) :
+      slider.classList.add('slider-08');
+      break;
+    case (parseFloat(slider.value) >= 0.7) :
+      slider.classList.add('slider-07');
+      break;
+    case (parseFloat(slider.value) >= 0.6) :
+      slider.classList.add('slider-06');
+      break;
+    case (parseFloat(slider.value) >= 0.5) :
+      slider.classList.add('slider-05');
+      break;
+  }
 }
