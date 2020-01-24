b0631a0a57f1d03cea75dfd17e3bd2c2580c6d3e
River Honer <river@someserver.de>
2020-01-24 16:57:04 +0100

    space for rules

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,29 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    // Bundles entry points for js for each part of web ext
+    // Content
     'content/opt-out-ext.js': './src/content/opt-out-ext.js',
+    // Popup
     'popup/popup-main.js': './src/popup/popup-main.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name]'
   },
-  // Copies static assets to correct place
+  module: {
+    rules: []
+  },
+  // Copies static assets to correct places
   plugins: [
     new CopyPlugin([
+      // Extension Static Assests
       { from: './src/index-1.html', to: './index-1.html' },
       { from: './src/manifest.json', to: './manifest.json' },
       { from: './src/README.md', to: './README.md' },
       { from: './src/icons', to: './icons' },
+      // Content Static Assets
       { from: './src/content/opt-out.css', to: './content/opt-out.css' },
+      // Popup Static Assets
       { from: './src/popup/assets', to: './popup/assets' },
       { from: './src/popup/popup.html', to: './popup/popup.html' }
     ])
