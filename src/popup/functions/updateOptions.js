56c6ca6ff2d0e955f449fa798a6007090ad26415
river <honer@heartbeat-med.de>
2020-04-21 21:08:04 +0200

    updateSliderKnob runs at correct time

--- src/popup/functions/updateOptions.js
+++ src/popup/functions/updateOptions.js
@@ -5,8 +5,21 @@ import updateSliderKnob from './updateSliderKnob.js';
  * @param result {object}
  */
 export default function (result) {
-  document.querySelector(`#${result.optOut.selector}`).checked = true;
   const slider = document.querySelector('#slider');
-  slider.value = result.optOut.slider;
-  updateSliderKnob(slider);
+  // If no saved settings in local storage
+  if (
+    Object.keys(result).length === 0 ||
+    result.optOut == null ||
+    result.optOut.slider == null ||
+    result.optOut.selector == null
+  ) {
+    // Set slider class for display
+    updateSliderKnob(slider);
+  } else {
+    // If there are saved settings
+    document.querySelector(`#${result.optOut.selector}`).checked = true;
+    // Set the settings on the slider
+    slider.value = result.optOut.slider;
+    updateSliderKnob(slider);
+  }
 }
