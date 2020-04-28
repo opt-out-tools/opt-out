90414309d6a3047d3fa94df988de594c8e13c16b
river <honer@heartbeat-med.de>
2020-04-28 19:21:39 +0200

    use positive case

--- src/popup/functions/updateOptions.js
+++ src/popup/functions/updateOptions.js
@@ -6,19 +6,19 @@ import updateSliderKnob from './updateSliderKnob.js';
  */
 export default function (result) {
   const slider = document.querySelector('#slider');
-  // If no saved settings in local storage
   if (
-    Object.keys(result).length === 0 ||
-    result.optOut == null ||
-    (result.optOut.slider == null && result.optOut.selector == null)
+    // There are saved settings
+    result &&
+    result.optOut &&
+    result.optOut.slider &&
+    result.optOut.selector
   ) {
-    // Set slider class for display
-    updateSliderKnob(slider);
-  } else {
-    // If there are saved settings
     document.querySelector(`#${result.optOut.selector}`).checked = true;
     // Set the settings on the slider
     slider.value = result.optOut.slider;
+    updateSliderKnob(slider);
+  } else {
+    // No saved settings
     updateSliderKnob(slider);
   }
 }
