f3be4f7f358059ca3d913c2a0132cf0eabe6f7e5
malteserteresa <tea.in.grams@gmail.com>
2020-06-09 20:46:02 +0200

    Revert "Extract the Twitter CSS selectors"

--- src/content/functions/styleTweet.js
+++ src/content/functions/styleTweet.js
@@ -1,11 +1,9 @@
-import { TWEET_CLASSES } from '../constants';
-
 /**
  * @description Sets display classes to tweet nodes when the text has been predicted
  * to be misogynist and when the options are set to block misogynist content.
  */
 export default function (element, popupPrefs) {
-  element.classList.remove(...Object.values(TWEET_CLASSES));
+  element.classList.remove('opt-out-tw', 'opt-out-tc', 'opt-out-trem');
   const tweetPredictionValue = parseFloat(element.dataset.prediction);
   const modifyTweetThreshold = parseFloat(popupPrefs.sliderVal);
   if (
@@ -14,11 +12,18 @@ export default function (element, popupPrefs) {
     // Option to modify tweet is turned on
     modifyTweetThreshold !== 0 &&
     // Tweets prediction value is enough to modify
-    tweetPredictionValue >= modifyTweetThreshold &&
-    // unill jest setup for null coalescing opeartor is added
-    TWEET_CLASSES[popupPrefs.optionVal] !== undefined
+    tweetPredictionValue >= modifyTweetThreshold
   ) {
-    element.classList.add(TWEET_CLASSES[popupPrefs.optionVal]);
-    // TODO: replace with element.classList.add(TWEET_CLASSES[popupPrefs.optionVal] ?? '');
+    switch (popupPrefs.optionVal) {
+      case 'text_white':
+        element.classList.add('opt-out-tw');
+        break;
+      case 'text_crossed':
+        element.classList.add('opt-out-tc');
+        break;
+      case 'text_removed':
+        element.classList.add('opt-out-trem');
+        break;
+    }
   }
 }
