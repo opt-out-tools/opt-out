42b6f33dac27fbc908155c89540bfbb2bd7206b1
Nikola Ribic <nikola.ribic@softerrific.com>
2020-06-17 11:08:34 +0200

    - decoupling function

--- src/content/functions/checkText.js
+++ src/content/functions/checkText.js
@@ -18,6 +18,13 @@ function handleErrors (response) {
   return response;
 }
 
+function handleResponse (response) {
+  return response;
+}
+
+// eslint-disable-next-line no-undef
+const handleRepsponseOrError = compose(handleErrors, handleResponse);
+
 /**
  * @description function which calls server for given node, and depending on the response,
  * applies pre-defined action
@@ -33,7 +40,29 @@ export default (node, selector, popupPrefs) => {
   );
   const text = tweetTextNode.innerText;
   const reqBody = { texts: [text] };
-  fetch(OPT_OUT_API_URL, {
+  postData(OPT_OUT_API_URL, reqBody).then(body => {
+    const predictions = body.predictions;
+    // If response contains prediction
+    if (predictions && predictions.length > 0) {
+      // Convert prediction state to int
+      const predictionInt = Number(predictions[0]);
+      // Add processing status and prediction to tweet node
+      node.classList.add('processed-true');
+      tweetTextNode.setAttribute(
+        'data-prediction',
+        predictionInt.toString()
+      );
+      styleTweet(tweetTextNode, popupPrefs);
+    } else {
+      // If no prediction
+      node.classList.add('processed-false');
+    }
+    node.classList.remove('processing');
+  });
+};
+
+async function postData (url = '', data = {}) {
+  const response = await fetch(url, {
     method: 'POST',
     mode: 'cors',
     credentials: 'include',
@@ -41,36 +70,13 @@ export default (node, selector, popupPrefs) => {
       'Content-Type': 'application/json;charset=UTF-8',
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify(reqBody)
+    body: JSON.stringify(data)
   })
-    .then(handleErrors)
-    .then(response => {
-      // Parse body json
-      response.json().then(body => {
-        const predictions = body.predictions;
-        // If response contains prediction
-        if (predictions && predictions.length > 0) {
-          // Convert prediction state to int
-          const predictionInt = Number(predictions[0]);
-          // Add processing status and prediction to tweet node
-          node.classList.add('processed-true');
-          tweetTextNode.setAttribute(
-            'data-prediction',
-            predictionInt.toString()
-          );
-          styleTweet(tweetTextNode, popupPrefs);
-        } else {
-          // If no prediction
-          node.classList.add('processed-false');
-        }
-      });
-      // Remove processing state from tweet
-      node.classList.remove('processing');
-    })
+    .then(handleRepsponseOrError)
     .catch(err => {
-      // Remove processing state from tweet
-      node.classList.add('processed-false');
-      node.classList.remove('processing');
-      console.error(err, node);
+      return {
+        error: err
+      };
     });
-};
+  return response.json();
+}
